17287bd8a0b44264f803b09ea3424cc5b21e1f57
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2023-10-10 14:24:42 +0200

    changed inhibition implementation, now it works

--- NerualAnnulus.py
+++ NerualAnnulus.py
@@ -16,28 +16,53 @@ N = 100
 J = 0.05
 K = 1.1
 
+#Some utilities
+def find_max(vector_state):
+    a = []
+    b = []
+    for i_state in vector_state:
+        i_list = i_state.tolist()
+        a.append(i_list.index(max(i_list[::4]))/4)
+        b.append(i_list.index(max(i_list[2::4]))/4)
+    resultArray = np.array([a,b])
+    return resultArray
+def account_cycles(vector, prev):
+    resultArray = [prev,]
+    for x in vector[1::]:
+        if (x < prev):
+            while (x < prev):
+                x += 100
+            resultArray.append(x)
+        else:
+            resultArray.append(x)
+        prev = x
+    return resultArray
+
 #Initial state and ODEs of the system
 mean = N/2
 sigma = N/100
-restState = [-a, -a + a*a*a/3]
+restState = [-a, -a + a*a*a/3, -a, -a + a*a*a/3]
 
-y0 = np.ndarray(2*N, float)
-for i in range(0, len(y0), 2):
-    y0[i] = 2*exp(-0.5*(i/2-mean)*(i/2-mean)/(sigma*sigma)) + restState[0]
+y0 = np.ndarray(4*N, float)
+for i in range(0, len(y0), 4):
+    y0[i] = 2*exp(-0.5*(i/4-mean/2)*(i/4-mean/2)/(sigma*sigma)) + restState[0]
     y0[i+1] = restState[1]
+    y0[i+2] =  restState[2]#2*exp(-0.5*(i/4-3*mean/2)*(i/4-3*mean/2)/(sigma*sigma)) +
+    y0[i+3] = restState[3]
 
 def f(t, y):
-    sysEquations = np.ndarray(2*N)
-    for i in range(0, len(y), 2):
+    sysEquations = np.ndarray(4*N)
+    for i in range(0, len(y), 4):
         if (i == 0):
-            sysEquations[i]=(y[i]-y[i]*y[i]*y[i]/3-y[i+1]+J*(y[2*N-2]-y[i]+y[i+2]))/e
+            sysEquations[i]=(y[i]-y[i]*y[i]*y[i]/3-y[i+1]+J*(y[4*N-4]-y[i])-K*(y[i+2]-y[i]))/e
             sysEquations[i+1]=y[i]+a
-        elif (i == 2*N-2):
-            sysEquations[i]=(y[i]-y[i]*y[i]*y[i]/3-y[i+1]+J*(y[i-2]-y[i]+y[0]))/e
-            sysEquations[i+1]=y[i]+a
+            sysEquations[i+2]=(y[i+2]-y[i+2]*y[i+2]*y[i+2]/3-y[i+3]+J*(y[4*N-2]-y[i+2])-K*(y[i]-y[i+2]))/e
+            sysEquations[i+3]=y[i+2]+a
         else:
-            sysEquations[i]=(y[i]-y[i]*y[i]*y[i]/3-y[i+1]+J*(y[i-2]-y[i]+y[i+2]))/e
+            sysEquations[i]=(y[i]-y[i]*y[i]*y[i]/3-y[i+1]+J*(y[i-4]-y[i])-K*(y[i+2]-y[i]))/e
             sysEquations[i+1]=y[i]+a
+            sysEquations[i+2]=(y[i+2]-y[i+2]*y[i+2]*y[i+2]/3-y[i+3]+J*(y[i-2]-y[i+2])-K*(y[i]-y[i+2]))/e
+            sysEquations[i+3]=y[i+2]+a
     return sysEquations
 
 """elif (i == 2*N-2):
@@ -62,24 +87,13 @@ while(True):
 t1 = tm.time()
 
 #Propagation speed calculations
-maxLocation = []
-for i_state in state:
-    i_list = i_state.tolist()
-    maxLocation.append((i_list.index(max(i_list[::2])))/2)
-"""
-prev=maxLocation[0]
-a = [prev,]
-for x in maxLocation[1::]:
-    if (x < prev):
-        while (x < prev):
-            x += 100
-        a.append(x)
-    else:
-        a.append(x)
-    prev = x
-regression = linregress(time, a)
-x_fit = [t*regression.slope + regression.intercept for t in time]
-"""
+tMax, sMax = find_max(state)
+target = np.array(account_cycles(tMax, tMax[0]))
+suppressed = np.array(account_cycles(sMax, sMax[0]))
+tRegression = linregress(time, target)
+sRegression = linregress(time, suppressed)
+t_fit = [t*tRegression.slope + tRegression.intercept for t in time]
+s_fit = [t*tRegression.slope + sRegression.intercept for t in time]
 t2 = tm.time()
 
 #Final drawings
@@ -88,32 +102,36 @@ print("Time elapsed during the calculation:", t1 - t0)
 print("Time elapsed for speed calculations:", t2 - t1)
 print("Dimesion of the system:\t", solution.n, "\nNumber of neurons per annulus:\t", N)
 print("Time:\t",T,"s\nTime steps:\t", len(time), "\nPlotted at:\t", i)
+print(tRegression,'\n',sRegression)
 #print("\nRegression result:\nSlope:\t",regression.slope,"\nIntercept:\t", regression.intercept,"\nR-value:\t",regression.rvalue)
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.grid()
-ax1.set_title("State of the sistem at time "+str(i)+"/"+str(len(time)))
-ax1.plot(state[i][::2], 'o', markersize=2)
-ax1.plot(state[i][1::2], 'o', markersize=2)
+#ax1.set_title("State of the sistem at time "+str(i)+"/"+str(len(time)))
+#ax1.plot(state[i][::4], 'o', markersize=2)
+#ax1.plot(state[i][2::4], 'o', markersize=2)
+plt.scatter([row for row in state], time, c=np.linspace(-5,5,100))
 ax2.grid()
 ax2.set_title("5th Neuron dynamics")
-ax2.plot(time, [row[10] for row in state])
-ax2.plot(time, [row[11] for row in state], '--')
-#plt.figure()
-#plt.title("Wave front propagation")
-#plt.grid()
-#plt.plot(time, a, '+')
-#plt.plot(time, x_fit)
+ax2.plot(time, [row[20] for row in state])
+ax2.plot(time, [row[22] for row in state])
 plt.figure()
-plt.title("Initial conditions")
-plt.plot(y0[::2], 'x')
-plt.plot(y0[1::2], 'x')
-plt.show()
+plt.title("Wave fronts propagation")
+plt.grid()
+plt.plot(time, target, 'c+')
+plt.plot(time, t_fit, 'c')
+plt.plot(time, suppressed, 'm+')
+plt.plot(time, s_fit, 'm')
+#plt.figure()
+#plt.title("Initial conditions")
+#plt.plot(y0[::4], 'x')
+#plt.plot(y0[2::4], 'x')
+#plt.show()
 
-#"""
 #Animation
 fig, ax = plt.subplots()
 ax.set_xlim(0, 100)
-ax.set_ylim(-3, 5)
+ax.set_ylim(-5, 5)
+ax.grid()
 fig.suptitle("Soliton wave animation")
 line1 = ax.plot()
 def init():
@@ -121,11 +139,12 @@ def init():
 def update(frame, data):
     ax.clear()
     ax.set_xlim(0, 100)
-    ax.set_ylim(-3, 5)
-    ax.plot(data[frame][::2], 'o', markersize=3)
-    ax.plot(data[frame][1::2], 'o', markersize=3)
+    ax.set_ylim(-5, 5)
+    ax.grid()
+    ax.plot(data[frame][::4], 'o', markersize=3)
+    ax.plot(data[frame][2::4], 'o', markersize=3)
 animation = FuncAnimation(fig, partial(update,data=state), frames=range(len(state)), init_func=init, interval=10)
-Writer = writers['pillow']
-writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
-animation.save('SolitonWave.gif', writer=writer)
-#"""
+#Writer = writers['ffmpeg']
+#writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
+#animation.save('SolitonWave.mp4', writer=writer)
+plt.show()
