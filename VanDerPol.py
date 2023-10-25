014a187ed9ac4e28ccf4766c65c1258c6014b69c
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2023-10-25 10:37:08 +0200

    van der pol

--- VanDerPol.py
+++ VanDerPol.py
@@ -3,31 +3,34 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import argparse
 import time as tm
+from math import cos,sqrt,exp
 
+INTEGRATORS = ['RK45','Radau','BDF']
 t_init = tm.time()
 parser = argparse.ArgumentParser()
 parser.add_argument('-mu', '--parameter', type=float, default=1)
 parser.add_argument('-tf', '--t_bound',type=float, default=10)
 parser.add_argument('-y0', '--initial_state', type=str, default='0.0,0.0')
+parser.add_argument('--integrator',type=str,choices=INTEGRATORS,default='Radau')
 args = parser.parse_args()
 mu = args.parameter
 tf = args.t_bound
+method = args.integrator
 y0 = np.array([float(x) for x in args.initial_state.split(',')])
-
 def van_der_pol(t,y):
     return np.array([y[1], mu*(1-y[0]*y[0])*y[1]-y[0]])
-def jacobian(t,y):
-    return np.array([[0,1],[-2*mu*y[0]*y[1]-1, mu*(1-y[0]*y[0])]])
-lintime = np.linspace(0,tf,int(tf*100))
 t_start = tm.time()
-solution = solve_ivp(van_der_pol,(0,tf),y0,'Radau',lintime,True,jac=jacobian)
+solution = solve_ivp(van_der_pol,(0,tf),y0,method,dense_output=True)
 t_calc = tm.time()
 print(t_start-t_init, t_calc-t_start,t_calc-t_init)
 time = solution.t
 state = solution.y
 fig, axes = plt.subplots(1,2)
+fig.suptitle("Van der Pol oscillator")
+axes[0].set_title("Phase plane")
+axes[1].set_title("Dynamics")
 axes[0].grid()
 axes[1].grid()
-axes[0].plot(state[0],state[1])
-axes[1].plot(time, state[0])
+axes[0].plot(state[0],state[1],color='tab:gray')
+axes[1].plot(time,state[0],color='tab:gray')
 plt.show()
